a5a3967cc6731ef985c00697bf526233801140ac
Ian caironi <iancaironi8@gmail.com>
2023-12-07 23:54:41 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Header from './components/layout/Header/Header';
+import Footer from './components/layout/Footer/Footer';
+import Nav from './components/layout/Nav/Nav';
+// import logo from './logo.svg';
 import './App.css';
+import HomePage from './pages/Home/HomePage';
+import NosotrosPage from './pages/Nosotros/NosotrosPages';
+import NovedadesPage from './pages/Novedades/NovedadesPage';
+import ContactoPage from './pages/Contacto/ContactoPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Header></Header>
+      <BrowserRouter>
+        <Nav></Nav>
+        <Routes>
+          <Route path="/" element={<HomePage></HomePage>}></Route>
+          <Route path="nosotros" element={<NosotrosPage></NosotrosPage>}></Route>
+          <Route path="novedades" element={<NovedadesPage></NovedadesPage>}></Route>
+          <Route path="contacto" element={<ContactoPage></ContactoPage>}></Route>
+        </Routes>
+      </BrowserRouter>
+      <Footer></Footer>
     </div>
   );
 }
